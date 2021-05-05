492edb9a10ff449a247f06d3244decf8d1676111
David <davidramirez9210@gmail.com>
2021-05-05 19:56:49 -0300

    Create Error component and implement in Formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -3,6 +3,7 @@ import styled from '@emotion/styled';
 import useMoneda from '../hooks/useMoneda';
 import useCriptomoneda from '../hooks/useCriptomoneda';
 import axios from 'axios';
+import Error from './Error';
 
 const Boton = styled.input `
     margin-top: 20px;
@@ -69,7 +70,7 @@ const Formulario = () => {
         <form
             onSubmit= {cotizarMoneda}
         >
-            {error&&"Hay un error"}
+            {error&&<Error mensaje="Todos los Campos son obligatorios"/>}
             <SelectMonedas />
 
             <SelectCripto />
