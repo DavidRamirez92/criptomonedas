1ef8640f53d29f2c7d6d578b6c107449c1010dfd
David <davidramirez9210@gmail.com>
2021-05-05 19:21:22 -0300

    useMoneda style component

--- src/hooks/useMoneda.jsx
+++ src/hooks/useMoneda.jsx
@@ -1,14 +1,35 @@
 import React,{useState} from 'react';
 import styled from '@emotion/styled';
 
+const Label = styled.label`
+    font-family: 'Bebas Neue', cursive;
+    color:#FFF;
+    text-transform:uppercase;
+    font-weight:bold;
+    font-size:2.4rem;
+    margin-top:2rem;
+    display:block;
+`;
+
+const Select = styled.select`
+    width:100%;
+    display:block;
+    padding:1rem;
+    -webkit-appearance: none;
+    border-radius:10px;
+    border:none;
+    font-size:1.2rem;
+
+`;
+
 const useMoneda = (label,stateInicial,opciones) => {
     //State de nuestro custom hook
     const [state,actualizarState] = useState(stateInicial);
     
     const Seleccionar = () => (
         <> 
-            <label>{label}</label>
-            <select
+            <Label>{label}</Label>
+            <Select
                 onChange={e => actualizarState(e.target.value)}
                 value={state}
             >
@@ -17,7 +38,7 @@ const useMoneda = (label,stateInicial,opciones) => {
                     <option key={opcion.codigo} value={opcion.codigo}>{opcion.nombre}</
                     option>
                 ))}
-            </select>
+            </Select>
         </>
     );
 
